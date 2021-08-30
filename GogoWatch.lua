a31f2601b41a91a271fcd43d4a6f9298399662a7
Arco Dielhof <arcodielhof@hotmail.com>
2021-08-30 11:37:54 +0200

    - Rename of Global GGWTest to GogoWatch. - Basic functionality for Dev on ToolTips. - Indentation Fixes.

--- GogoWatch.lua
+++ GogoWatch.lua
@@ -1,33 +1,40 @@
-if GGWTest == nil then GGWTest = {} end
-GGWTest.Events = {}
+if GogoWatch == nil then GogoWatch = {} end
+GogoWatch.Events = {}
 
 local EventFrame = nil
 
 local addonLoaded, variablesLoaded = false, false
 
-function GGWTest:OnLoad()
+function GogoWatch:OnLoad()
     EventFrame = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
     EventFrame:RegisterEvent("VARIABLES_LOADED")
     EventFrame:RegisterEvent("ADDON_LOADED")
     EventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-    EventFrame:SetScript("OnEvent", function(...) GGWTest:OnEvent(...) end)
+    EventFrame:SetScript("OnEvent", function(...) GogoWatch:OnEvent(...) end)
+
+    GameTooltip:HookScript("OnTooltipSetUnit", function(...)
+        local curMouseOver = UnitGUID("MouseOver")
+        if GogoWatch.Devs[curMouseOver] == true then
+            GameTooltip:AddLine(GogoWatch.Strings.TeamMeberToolTip)
+        end
+    end)
 end
 
-function GGWTest:OnEvent(self, event, ...)
+function GogoWatch:OnEvent(self, event, ...)
     if event == "VARIABLES_LOADED" then
-        GGWTest.Events:VariablesLoaded(...)
+        GogoWatch.Events:VariablesLoaded(...)
     elseif event == "ADDON_LOADED" then
-        GGWTest.Events:AddonLoaded(...)
+        GogoWatch.Events:AddonLoaded(...)
     elseif event == "COMBAT_LOG_EVENT_UNFILTERED" then
-        GGWTest.Events:CombatLogEventUnfiltered(...)
+        GogoWatch.Events:CombatLogEventUnfiltered(...)
     end
 end
 
-function GGWTest.Events:CombatLogEventUnfiltered(...)
+function GogoWatch.Events:CombatLogEventUnfiltered(...)
     local _, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellID, spellName, spellSchool = CombatLogGetCurrentEventInfo()
     if subevent == "SPELL_CAST_SUCCESS" and sourceGUID == UnitGUID("Player") then
-        local curSpell = GGWTest.SpellIDs[spellID]
-        local Strings = GGWTest.Strings
+        local curSpell = GogoWatch.SpellIDs[spellID]
+        local Strings = GogoWatch.Strings
         if curSpell == nil then
             print(string.format(Strings.tempDevCastString, spellName, spellID))
         else
@@ -47,19 +54,19 @@ function GGWTest.Events:CombatLogEventUnfiltered(...)
     end
 end
 
-function GGWTest.Events:VarsAndAddonLoaded()
+function GogoWatch.Events:VarsAndAddonLoaded()
 
 end
 
-function GGWTest.Events:AddonLoaded(...)
+function GogoWatch.Events:AddonLoaded(...)
     local addonName = ...
     if addonName == "GogoWatch" then
-        if variablesLoaded == true then GGWTest.Events:VarsAndAddonLoaded() else addonLoaded = true end
+        if variablesLoaded == true then GogoWatch.Events:VarsAndAddonLoaded() else addonLoaded = true end
     end
 end
 
-function GGWTest.Events:VariablesLoaded(...)
-    if addonLoaded == true then GGWTest.Events:VarsAndAddonLoaded() else variablesLoaded = true end
+function GogoWatch.Events:VariablesLoaded(...)
+    if addonLoaded == true then GogoWatch.Events:VarsAndAddonLoaded() else variablesLoaded = true end
 end
 
-GGWTest:OnLoad()
+GogoWatch:OnLoad()
